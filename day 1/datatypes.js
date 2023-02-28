cd94a5c5c3d06d66c6f78f2ec4998929a9190e46
OmoItohan <itohanomovbude@gmail.com>
2023-02-28 22:12:36 +0100

    completed day1 and 2 exercise

---
+++ day 1/datatypes.js
@@ -0,0 +1,43 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let myName = 'Aity',
+    single = true,
+    workingCondition ,
+    phoneNumber = null;
+
+typeof myName // string
+typeof single // boolean
+typeof workingCondition //undefined
+typeof phoneNumber //null
+
+//Declare four variables without assigning values
+let coder = '',
+    den = '',
+    beginners = '',
+    intermediate = ''
+
+//Declare four variables with assigned values
+let Coder = 'shola',
+    Den = 'Ebuka',
+    Beginners = 21,
+    Intermediate = 53
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'Itohan'
+let lastNAme = 'Omovbude'
+let maritalStatus = 'Single'
+let country = 'Nigeria'
+let age = 24
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let FirstName = 'Itohan',
+    LastNAme = 'Omovbude',
+    MaritalStatus = 'Single',
+    Country = 'Nigeria',
+    Age = 24
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 25
+let yourAge = 30
+
+console.log('I am ' + myAge + ' years old')
+console.log('You are ' + yourAge + ' years old')
---
+++ day 1/variable.js
@@ -0,0 +1,6 @@
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let myName = 'Aity',
+    single = false,
+    workingCondition = undefined,
+    phoneNumber = null;
+
